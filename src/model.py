283043708cf565d2cc56805ccc9f7698e2f538ed
Amadeus <amadeusw6@gmail.com>
2023-12-30 15:02:20 +0800

    Format model.py

--- src/model.py
+++ src/model.py
@@ -1,38 +1,46 @@
 import torch
 import torch.nn as nn
 
+
 def PE(x, degree):
-    y = torch.cat([2.**i * x for i in range(degree)], -1)
+    y = torch.cat([2.0**i * x for i in range(degree)], -1)
     w = 1
     return torch.cat([x] + [torch.sin(y) * w, torch.cos(y) * w], -1)
 
 
 class CodeNeRF(nn.Module):
-    def __init__(self, shape_blocks = 2, texture_blocks = 1, W = 256, 
-                 num_xyz_freq = 10, num_dir_freq = 4, latent_dim=256):
+    def __init__(
+        self,
+        shape_blocks=2,
+        texture_blocks=1,
+        W=256,
+        num_xyz_freq=10,
+        num_dir_freq=4,
+        latent_dim=256,
+    ):
         super().__init__()
         self.shape_blocks = shape_blocks
         self.texture_blocks = texture_blocks
         self.num_xyz_freq = num_xyz_freq
         self.num_dir_freq = num_dir_freq
-        
+
         d_xyz, d_viewdir = 3 + 6 * num_xyz_freq, 3 + 6 * num_dir_freq
         self.encoding_xyz = nn.Sequential(nn.Linear(d_xyz, W), nn.ReLU())
         for j in range(shape_blocks):
-            layer = nn.Sequential(nn.Linear(latent_dim,W),nn.ReLU())
+            layer = nn.Sequential(nn.Linear(latent_dim, W), nn.ReLU())
             setattr(self, f"shape_latent_layer_{j+1}", layer)
-            layer = nn.Sequential(nn.Linear(W,W), nn.ReLU())
+            layer = nn.Sequential(nn.Linear(W, W), nn.ReLU())
             setattr(self, f"shape_layer_{j+1}", layer)
-        self.encoding_shape = nn.Linear(W,W)
-        self.sigma = nn.Sequential(nn.Linear(W,1), nn.Softplus())
-        self.encoding_viewdir = nn.Sequential(nn.Linear(W+d_viewdir, W), nn.ReLU())
+        self.encoding_shape = nn.Linear(W, W)
+        self.sigma = nn.Sequential(nn.Linear(W, 1), nn.Softplus())
+        self.encoding_viewdir = nn.Sequential(nn.Linear(W + d_viewdir, W), nn.ReLU())
         for j in range(texture_blocks):
             layer = nn.Sequential(nn.Linear(latent_dim, W), nn.ReLU())
             setattr(self, f"texture_latent_layer_{j+1}", layer)
-            layer = nn.Sequential(nn.Linear(W,W), nn.ReLU())
+            layer = nn.Sequential(nn.Linear(W, W), nn.ReLU())
             setattr(self, f"texture_layer_{j+1}", layer)
-        self.rgb = nn.Sequential(nn.Linear(W, W//2), nn.ReLU(), nn.Linear(W//2, 3))
-        
+        self.rgb = nn.Sequential(nn.Linear(W, W // 2), nn.ReLU(), nn.Linear(W // 2, 3))
+
     def forward(self, xyz, viewdir, shape_latent, texture_latent):
         xyz = PE(xyz, self.num_xyz_freq)
         viewdir = PE(viewdir, self.num_dir_freq)
